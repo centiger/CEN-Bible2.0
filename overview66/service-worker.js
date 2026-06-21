669fee2bb66b04a9ee78f894dea56bf5d989c490
centiger <centiger2@gmail.com>
2026-06-21 09:24:06 +0900

    Add files via upload

--- overview66/service-worker.js
+++ overview66/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cen-bible-2-0-soft-overview-20260513-onebible-fix';
+const CACHE_NAME = 'cen-bible-2-0-overview66-homeback-20260621';
 const ASSETS=['./','./index.html','./manifest.json','./data/books.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
